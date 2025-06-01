17169d19669df509eb406f5d775f9ff647bcfbbb
Atiimubeen <atii237237@gmail.com>
2025-06-02 00:00:22 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo by Atif Mubeen',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+
+  String _currentFortune = "";
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _currentFortune = "";
+  final _fortuneList = [
+    "You will find the friend",
+    "You will find friend tomorrow",
+    "Hi this is Atif mubeen",
+    "Muhammad zaheem Aslam",
+    "Hi, this is Shahroz aslam",
+  ];
+
+  void _randomFortune() {
+    var random = Random();
+    int fortune = random.nextInt(_fortuneList.length);
+    setState(() {
+      _currentFortune = _fortuneList[fortune];
+    });
+
+    print(_currentFortune);
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text("Flutter app development by Atif Mubeen"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Your fortune is: ',
+              style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '${_currentFortune}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ),
+            ),
+            ElevatedButton(onPressed: _randomFortune, child: Text("press me!")),
+          ],
+        ),
+      ),
+      //   floatingActionButton: FloatingActionButton(
+      //     onPressed: _randomFortune,
+      //     tooltip: 'Increment',
+      //     child: const Icon(Icons.add),
+      //   ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
